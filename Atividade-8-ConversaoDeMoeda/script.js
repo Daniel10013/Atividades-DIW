acc954205a7b2095f47af97c3b62f48b38296b27
Daniel Filipe <daniel.filipesorescv@gmail.com>
2024-05-08 19:31:41 -0300

    Finalizando atividade

--- Atividade-8-ConversaoDeMoeda/script.js
+++ Atividade-8-ConversaoDeMoeda/script.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", function(){
+    var audio = new Audio('./assets/sound/windows_boot_sound.mp3');
+    audio.play();
+})
+
 
 function refreshTime() {
     var timeDisplay = document.getElementById("time");
@@ -7,3 +12,32 @@ function refreshTime() {
 }
 
 setInterval(refreshTime, 1000);
+
+document.getElementById("calc-button").addEventListener("click", function(){
+
+    let realInput = document.getElementById("real-value");
+    let realValue = realInput.value;
+
+    if(realValue == 0 || isNaN(realValue)){
+        document.querySelector(".result-box").innerHTML = "Valor inválido digitado!"
+        return;
+    }
+
+    let currencesValues = {
+        "dolar": "US$"+ formatValue(realValue / 5.09), 
+        "euro": "€"+ formatValue(realValue / 5.47),
+        "iene": "¥"+ formatValue(realValue / 0.033),
+        "libra": "£"+ formatValue(realValue / 6.36),
+        "franco": "Fr"+ formatValue(realValue / 5.60),
+    }
+
+    let selectedOption = document.getElementById("currence-option").value;
+    let conversionValue = currencesValues[selectedOption];
+    let result = conversionValue + "<br> Com base na cotação do dia 08/05/2024";
+
+    document.querySelector(".result-box").innerHTML = result;
+})
+
+function formatValue(number){
+    return Number(number).toFixed(2);
+}
